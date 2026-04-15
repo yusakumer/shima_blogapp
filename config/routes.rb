4629ab350fa697744a021f8f909da0f37f3bab00
yusaku <s.yusaku@gmail.com>
2026-04-16 06:51:55 +0900

    rubyメソッドでのlink作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root to: "home#index"
   get "/" => "home#index"
 
   get "/about" => "home#about"
