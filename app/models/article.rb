296e9c6bb1f21c2fab791008c2e59802b96ab305
yusaku <s.yusaku1220@gmail.com>
2026-04-24 12:44:53 +0900

    validation追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,7 +10,11 @@
 #
 class Article < ApplicationRecord
   validates :title, presence: true
+  validates :title, length: { minimum: 2}
+
+  # length
   validates :content, presence: true
+  validates :content, length: { minimum: 10}
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
