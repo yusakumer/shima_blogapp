f20e168e30a23a962e52e8ac4cf76318cedbbaff
yusaku <s.yusaku1220@gmail.com>
2026-04-24 12:57:06 +0900

    uniqueness追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,11 +10,12 @@
 #
 class Article < ApplicationRecord
   validates :title, presence: true
-  validates :title, length: { minimum: 2}
+  validates :title, length: { minimum: 2, maximum: 100 }
 
   # length
   validates :content, presence: true
-  validates :content, length: { minimum: 10}
+  validates :content, length: { minimum: 10, maximum: 1000 }
+  validates :content, uniqueness: true
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
