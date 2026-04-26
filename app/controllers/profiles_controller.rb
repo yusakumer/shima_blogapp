f25665025320c6901d9b5e523fa6dc5c5b5a13bb
yusaku <s.yusaku1220@gmail.com>
2026-04-26 16:45:35 +0900

    gender selectbox追加 enum管理

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,12 @@
 class ProfilesController < ApplicationController
 
   before_action :authenticate_user!
+
   def show
     @profile = current_user.profile
   end
 
   def edit
-
+    @profile = current_user.build_profile
   end
 end
