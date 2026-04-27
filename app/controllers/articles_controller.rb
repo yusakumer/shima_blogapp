50ff20e2b6c152cc0cb666e34af0d25e9bf36306
yusaku <s.yusaku1220@gmail.com>
2026-04-28 00:18:40 +0900

    eyecatch追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content, :eyecatch)
   end
 
   def set_article
