516a9664fa3c0827998abe58bf957e8065199267
yusaku <s.yusaku1220@gmail.com>
2026-05-01 23:29:31 +0900

    reletionship再定義user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,10 @@ class User < ApplicationRecord
 
   has_many :following_relationships, foreign_key: :"follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: :"following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   delegate :birthday, :age, :gender, to: :profile, allow_nil: true
@@ -46,6 +50,10 @@ class User < ApplicationRecord
 
   def follow!(user)
     following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    following_relationships.find_by!(following_id: user.id).destroy!
   end
 
   # def birthday
