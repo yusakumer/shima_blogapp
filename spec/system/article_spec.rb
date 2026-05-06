0a2560cf43d76618db993553147c5c000e4827e6
yusaku <s.yusaku1220@gmail.com>
2026-05-06 21:15:41 +0900

    rspec profile 追加,usercreate and profile create

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -7,8 +7,16 @@ RSpec.describe "Article", type: :system do
   it "記事一覧が表示される" do
     visit root_path
     articles.each do |article|
-      expect(page).to have_content(article.title)
+      expect(page).to have_css(".card-title", text: article.title)
     end
 
+  end
+
+  it "記事の詳細を表示できる" do
+    visit root_path
+    article = articles.first
+    click_on article.title
+    expect(page).to have_css(".article_title", text: article.title)
+    expect(page).to have_css(".article_content", text: article.content.to_plain_text)
   end
 end
